e67d05ba725aa67540855e362df3ce90b36008c5
Gammani <shtucer31@gmail.com>
2024-04-18 12:46:05 +0300

    fix

--- src/features/users/application/users.service.ts
+++ src/features/users/application/users.service.ts
@@ -38,52 +38,9 @@ export class UsersService {
     );
     return await this.usersRepository.createUser(createdUser);
   }
-  //  async createUser(
-  //    inputUserModel: UserCreateModel,
-  //  ): Promise<CreatedUserViewModel | null> {
-  //    const passwordHash = await this.passwordAdapter.createPasswordHash(
-  //      inputUserModel.password,
-  //    );
-  //    const confirmationCode = uuidv4();
-  //    const createUser = this.UserModel.createUser(
-  //      inputUserModel,
-  //      passwordHash,
-  //      this.UserModel,
-  //      false,
-  //      confirmationCode,
-  //    );
-  //
-  //    const createdUser = await this.usersRepository.createUser(createUser);
-  //    try {
-  //      await this.emailManager.sendEmail(
-  //        inputUserModel.email,
-  //        inputUserModel.login,
-  //        `\` <h1>Thank for your registration</h1>
-  // <p>To finish registration please follow the link below:
-  //     <a href='https://somesite.com/confirm-email?code=${confirmationCode}'>complete registration</a>
-  // </p>\``,
-  //      );
-  //    } catch (error) {
-  //      console.log(error);
-  //      await this.usersRepository.deleteUser(createdUser.id);
-  //      throw new ServiceUnavailableException();
-  //    }
-  //    return createdUser;
-  //  }
   async findUserById(userId: string): Promise<UserDbType | null> {
     return this.usersRepository.findUserById(userId);
   }
-  // async findUserViewModelByDeviceId(
-  //   deviceId: ObjectId,
-  // ): Promise<UserViewModel | null> {
-  //   const userId =
-  //     await this.securityDevicesService.findUserIdByDeviceId(deviceId);
-  //   if (userId) {
-  //     return await this.usersQueryRepository.findUserById(userId);
-  //   } else {
-  //     return null;
-  //   }
-  // }
   async findUserByDeviceId(deviceId: ObjectId): Promise<UserDbType | null> {
     const userId =
       await this.securityDevicesService.findUserIdByDeviceId(deviceId);
@@ -121,17 +78,6 @@ export class UsersService {
       return null;
     }
   }
-  // async updatePassword(newPassword: string, recoveryCode: string) {
-  //   debugger;
-  //   const foundUser = await this.findUserByRecoveryCode(recoveryCode);
-  //   if (foundUser) {
-  //     const passwordHash =
-  //       await this.passwordAdapter.createPasswordHash(newPassword);
-  //
-  //     return this.usersRepository.updatePassword(passwordHash, recoveryCode);
-  //   }
-  //   return;
-  // }
   async emailIsExist(email: string): Promise<boolean> {
     return await this.usersRepository.emailIsExist(email);
   }
